2fdcd9fe822b4b723fca49c360c276b5869f1c83
Peter Boothe <pboothe@google.com>
2018-12-21 16:23:15 -0500

    Refactor gatherers.

--- main_test.go
+++ main_test.go
@@ -8,6 +8,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/m-lab/nodeinfo/data"
+
 	"github.com/m-lab/go/osx"
 	"github.com/m-lab/go/rtx"
 )
@@ -19,11 +21,25 @@ func TestMainOnce(t *testing.T) {
 	rtx.Must(err, "Could not create tempdir")
 	revertDatadir := osx.MustSetenv("DATADIR", dir)
 	revertOnce := osx.MustSetenv("ONCE", "true")
+	og := gatherers
+	gatherers = []data.Gatherer{
+		{
+			Datatype: "uname",
+			Filename: "uname.txt",
+			Cmd:      []string{"uname", "-a"},
+		},
+		{
+			Datatype: "ifconfig",
+			Filename: "ifconfig.txt",
+			Cmd:      []string{"ifconfig", "-a"},
+		},
+	}
 	defer func() {
 		revertOnce()
 		revertDatadir()
 		os.RemoveAll(dir)
 		cancel()
+		gatherers = og
 	}()
 
 	// Run main.
@@ -49,10 +65,24 @@ func TestMainMultiple(t *testing.T) {
 	rtx.Must(err, "Could not create tempdir")
 	revertDatadir := osx.MustSetenv("DATADIR", dir)
 	revertOnce := osx.MustSetenv("WAIT", "10ms")
+	og := gatherers
+	gatherers = []data.Gatherer{
+		{
+			Datatype: "uname",
+			Filename: "uname.txt",
+			Cmd:      []string{"uname", "-a"},
+		},
+		{
+			Datatype: "ifconfig",
+			Filename: "ifconfig.txt",
+			Cmd:      []string{"ifconfig", "-a"},
+		},
+	}
 	defer func() {
 		revertOnce()
 		revertDatadir()
 		os.RemoveAll(dir)
+		gatherers = og
 	}()
 
 	// Run main but sleep for .5 seconds to guarantee that the timer will go off on
